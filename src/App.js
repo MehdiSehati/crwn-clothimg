e9c6c3186272a339ce2cc27099c7e1942f657915
MehdiSehati <sehati.mehdi@gmail.com>
2020-10-16 07:54:20 +0330

    added redux, and implemented userReducer and userActions to header and app component

--- src/App.js
+++ src/App.js
@@ -5,38 +5,30 @@ import { Switch,  Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import SingInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-
+import { connect } from 'react-redux';
 import {auth , createUserProfileDocument}  from './firebase/firebase.utils';
-
+import { setCurrentUser } from './Redux/user/user.actions';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state ={
-      currentUser: null
-    };
-    
-  }
+
   unsubscribeFromAuth=null
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
+        setCurrentUser({
               id: snapshot.id,
               ...snapshot.data()
-            }
+            });
           });
-          console.log(this.state)
-        });
+       
+       
         
       }
-      this.setState({
-        currentUser : userAuth
-      });
+     setCurrentUser(userAuth);
     });
   }
   componentWillUnmount(){
@@ -45,7 +37,7 @@ class App extends React.Component {
   render(){ 
      return (
     <div>
-      <Header currentUser={this.state.currentUser}/>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
@@ -56,4 +48,7 @@ class App extends React.Component {
 }
 }
 
-export default App;
+const mapDispatchToProps = dispatch =>({
+setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(null,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './Redux/store';
 ReactDOM.render(
+
+  <Provider store={store}>
   <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
 
-  </BrowserRouter>,
-  
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
