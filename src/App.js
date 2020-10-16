00f1b37a0d728438c1b0042a943dac4b8eb2d2a9
MehdiSehati <sehati.mehdi@gmail.com>
2020-10-16 11:15:01 +0330

    updatin our app if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Switch,  Route } from 'react-router-dom';
+import { Switch,  Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import SingInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
@@ -41,14 +41,27 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SingInAndSignUpPage} />
+        <Route exact path='/signin' render= {()=>
+        this.props.currentUser ? (
+          <Redirect to='/' />
+        ) : (
+          <SingInAndSignUpPage />
+        )
+
+        } />
       </Switch>
     </div>
   );
 }
 }
 
+const mapStateToProp = ({ user }) => ({ 
+ currentUser : user.currentUser
+});
 const mapDispatchToProps = dispatch =>({
 setCurrentUser: user => dispatch(setCurrentUser(user))
-})
-export default connect(null,mapDispatchToProps)(App);
+});
+export default connect(
+  mapStateToProp,
+  mapDispatchToProps
+  )(App);
